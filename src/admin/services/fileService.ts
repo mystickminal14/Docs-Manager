dc3180502a2d5c14a58389f85e9f02733a337ff5
mystick minal <mystick14minal@gmail.com>
2025-11-07 16:12:18 +0545

    done

--- src/admin/services/fileService.ts
+++ src/admin/services/fileService.ts
@@ -19,11 +19,11 @@ export const DeleteFileEndpoint = (fileShareId: string) => {
 };
 
 export const ChangeAccessTypeEndpoint = (fileShareId: string, accessType: "Public" | "Private") => {
-  const client = new APIClient<FileModel>(`/files/${fileShareId}/access-type`);
+  const client = new APIClient<FileModel>(`/files/update-access/${fileShareId}`);
   return client.put({ accessType } as any);
 };
 
 export const ChangeDisplayNameEndpoint = (fileShareId: string, displayName: string) => {
-  const client = new APIClient<{ message: string }>(`/files/${fileShareId}/display-name`);
+  const client = new APIClient<{ message: string }>(`/files/display-name/${fileShareId}`);
   return client.put({ displayName } as any);
 };
